069446aadcbcf29ca7359964f4527a527da7e805
Harshit Gupta <8318harshit@gmail.com>
2026-08-11 13:09:29 +0530

    Update Next.js deployment config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/task-manager',
-  assetPrefix: '/task-manager/',
-}
+const nextConfig = {}
 
 module.exports = nextConfig
